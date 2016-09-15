6a2a46a13c28ef48dc5b4f66cff2387cbac754f7
Daniel Cleaves <danielcleaves@actioncoach.com>
2016-09-14 20:05:46 -0500

    Added Colors and Seque

--- RGB-Sliders/ViewController.swift
+++ RGB-Sliders/ViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    @IBOutlet var colorSquare: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+         // Do any additional setup after loading the view, typically from a nib.
+        
+        updateBackgroundColor()
+        
+        colorSquare.layer.borderColor = UIColor.blackColor().CGColor
+        colorSquare.layer.borderWidth = 1
+       
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +31,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func updateBackgroundColor() {
+        let red = CGFloat(redSlider.value)
+        let green = CGFloat(greenSlider.value)
+        let blue = CGFloat(blueSlider.value)
+        
+        colorSquare.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        
+    }
 
 }
 
